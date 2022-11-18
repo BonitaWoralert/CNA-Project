514f647df9eddacacfd326d2fc65aa7ef3c3b4e8
Bonita Woralert <67742281+BonitaWoralert@users.noreply.github.com>
2022-11-18 17:08:16 +0000

    started ui

--- CNA/CNA/MainWindow.xaml.cs
+++ CNA/CNA/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CNA
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
